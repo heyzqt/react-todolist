945e24858e558566637fa4ff299f6093c8bd2fa2
zhangqing <heyzqt@qq.com>
2020-04-21 22:53:01 +0800

    TransitionGroup,为一个数组添加css动画

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import React, { Fragment } from "react";
 import "./style.css";
-import { CSSTransition } from "react-transition-group";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isShow: true,
+      list: [],
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.handleAddClick = this.handleAddClick.bind(this);
   }
 
-  handleClick() {
+  handleAddClick() {
     this.setState({
-      isShow: !this.state.isShow,
+      list: [...this.state.list, "item"],
     });
   }
 
   render() {
     return (
       <Fragment>
-        <CSSTransition
-          in={this.state.isShow}
-          timeout={300}
-          unmountOnExit
-          onEnter={(el) => {
-            el.style.color = "red";
-          }}
-          classNames="fade"
-          appear={true}
-        >
-          <div>hello world</div>
-        </CSSTransition>
-        <button onClick={this.handleClick}>change state</button>
+        <TransitionGroup>
+          {this.state.list.map((item, index) => {
+            return (
+              <CSSTransition
+                in={this.state.isShow}
+                timeout={300}
+                unmountOnExit
+                onEnter={(el) => {
+                  el.style.color = "red";
+                }}
+                classNames="fade"
+                appear={true}
+                key={index}
+              >
+                <div>{item}</div>
+              </CSSTransition>
+            );
+          })}
+        </TransitionGroup>
+        <button onClick={this.handleAddClick}>add Item</button>
       </Fragment>
     );
   }
