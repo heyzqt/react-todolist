cad7218211b61906c640b6a4b1822d2675b17495
heyzqt <heyzqt@qq.com>
2020-04-23 08:13:40 +0800

    添加redux-thunk模块,可以在action中添加异步请求,使得action返回函数而不是对象

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import Reducer from "./reducer";
+import thunk from "redux-thunk";
 
-const store = createStore(
-  Reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+//添加redux-devtools调试工具
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(Reducer, enhancer);
 
 export default store;
