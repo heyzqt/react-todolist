fa1f03c69e15f12692168762db1a0b143ace51bf
heyzqt <heyzqt@qq.com>
2020-04-24 00:39:04 +0800

    删除所有代码,引入react-redux模块

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ToDoList from "./TodoList";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ToDoList />
   </React.StrictMode>,
   document.getElementById("root")
 );
